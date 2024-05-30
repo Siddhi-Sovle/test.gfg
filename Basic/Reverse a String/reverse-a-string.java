dc719d366c1856ee95fbe87fa2130aabaa5ee550
Siddhi-Sovle <116745430+Siddhi-Sovle@users.noreply.github.com>
2024-05-30 22:00:34 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Reverse a String/reverse-a-string.java
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Driver
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        
+        while(t-- >0)
+        {
+            String str = read.readLine();
+            System.out.println(new Reverse().reverseWord(str));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Reverse
+{
+    // Complete the function
+    // str: input string
+    public static String reverseWord(String str)
+    {
+        StringBuilder sb = new StringBuilder(str);
+        
+        for (int i = 0; i < sb.length() / 2; i++) {
+            int front = i;
+            int back = sb.length() - 1 - i;
+            
+            char frontChar = sb.charAt(front);
+            char backChar = sb.charAt(back);
+            
+            sb.setCharAt(front, backChar);
+            sb.setCharAt(back, frontChar);
+        }
+        
+        return sb.toString();
+        
+    }
+}
